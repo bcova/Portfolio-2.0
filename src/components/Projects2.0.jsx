b100a125bc5adf3c74661ceed39bc310f35be4ed
bcova <brandoncova@gmail.com>
2023-07-15 20:43:05 -0500

    added my game to projects

--- src/components/Projects2.0.jsx
+++ src/components/Projects2.0.jsx
@@ -3,6 +3,7 @@ import todo from "../assets/todo.png";
 import login from "../assets/login.png";
 import weatherApp from "../assets/weatherApp.png";
 import tictac from "../assets/tictac.png";
+import game from '../assets/game.png'
 
 const Projects2 = () => {
   return (
@@ -16,7 +17,7 @@ const Projects2 = () => {
   <div id="slide1" className="carousel-item relative w-full overflow-clip">
     <img src={todo} className="object-cover scale-[1.3] relative top-9" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide4" className="btn btn-circle btn-primary">❮</a> 
+      <a href="#slide5" className="btn btn-circle btn-primary">❮</a> 
       <a href="#slide2" className="btn btn-circle btn-primary">❯</a>
     </div>
     <div className="absolute bottom-3 w-full flex justify-center gap-7 ">
@@ -53,13 +54,25 @@ const Projects2 = () => {
     <img src={tictac} className="object-cover" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide3" className="btn btn-circle btn-primary">❮</a> 
-      <a href="#slide1" className="btn btn-circle btn-primary">❯</a>
+      <a href="#slide5" className="btn btn-circle btn-primary">❯</a>
     </div>
     <div className="absolute bottom-3 w-full flex justify-center gap-7">
       <a href="https://github.com/bcova/Tic-Tac-Toe" className="btn btn-primary">Code</a>
       <a href="https://frabjous-bavarois-909bfa.netlify.app" className="btn btn-primary">Site</a>
     </div>
     <div className="w-fit h-fit absolute top-0 left-0 bg-primary rounded-tr-md rounded-br-md p-1">Tic-Tac-Toe App</div>
+  </div>
+  <div id="slide5" className="carousel-item relative w-full">
+    <img src={game} className="object-cover" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle btn-primary">❮</a> 
+      <a href="#slide1" className="btn btn-circle btn-primary">❯</a>
+    </div>
+    <div className="absolute bottom-3 w-full flex justify-center gap-7">
+      <a href="https://github.com/bcova/Game" className="btn btn-primary">Code</a>
+      <a href="https://transcendent-salmiakki-f31590.netlify.app/" className="btn btn-primary">Site</a>
+    </div>
+    <div className="w-fit h-fit absolute top-0 left-0 bg-primary rounded-tr-md rounded-br-md p-1">Game</div>
   </div>
 </div>
 </fieldset>
